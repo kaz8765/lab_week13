0ae35e8d59f9363e517d10dc4d8e3581bedfb9bc
Kazi <kazi_w@hotmail.com>
2022-04-01 21:15:30 -0700

    add CSS styling to background

--- src/app.js
+++ src/app.js
@@ -1,12 +1,26 @@
 "use strict";
+//create copy
 
+
+
+const selectItem = document.querySelector('.originals')
 const makeDuplicate = document.querySelector('.copy-container')
-
-const selectItem = document.querySelector('.original-container')
 
 function copydiv(evt){
     selectItem.addEventListener("click", (evt) => {
+    
     makeDuplicate.insertAdjacentHTML("afterbegin", evt.target.innerHTML)
+    
+})
+}
+copydiv() //makeDuplicate
+
+//delete all copy 
+
+function flushAllItems(evt){
+    const clear_copies = document.querySelector("button");
+    clear_copies.addEventListener("click", (evt) => {
+    makeDuplicate.innerHTML = "";
     })
 }
-copydiv(makeDuplicate)
+flushAllItems(makeDuplicate)
